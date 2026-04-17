d3eeb06afe8ce38856a9f3f68adf76a2b6449833
apoorva nayak <nayakapoorva2010@gmail.com>
2026-04-17 15:02:03 +0530

    /

--- app/(site)/page.jsx
+++ app/(site)/page.jsx
@@ -14,7 +14,7 @@ const HomePage = () => {
       <FeaturedCollection />
       <BestSeller />
       <BudgetGrid />
-      <VideoBannerWithSlider />
+      {/* <VideoBannerWithSlider /> */}
       <Testimonials />
     </section>
   );
